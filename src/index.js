79c4b90622787ad0b69896b420b53805aa077945
spences10 <spences10apps@gmail.com>
2019-06-04 20:48:38 +0100

    add examples from mdx-deck

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import { slides as one } from './appear/deck.mdx'
-import { slides as two } from './aspect-ratio/deck.mdx'
+import { slides as one } from "./appear/deck.mdx";
+import { slides as two } from "./aspect-ratio/deck.mdx";
+import { slides as three } from "./basic/deck.mdx";
+import { slides as four } from "./head/deck.mdx";
+import { slides as five } from "./images/deck.mdx";
+import { slides as six } from "./layouts/deck.mdx";
+import { slides as seven } from "./prism/deck.mdx";
+import { slides as eight } from "./provider/deck.mdx";
+import { slides as nine } from "./syntax-highlighting/deck.mdx";
+import { slides as ten } from "./themes/deck.mdx";
 
-export const slides = [...one, ...two]
+export const slides = [
+  ...one,
+  ...two,
+  ...three,
+  ...four,
+  ...five,
+  ...six,
+  ...seven,
+  ...eight,
+  ...nine,
+  ...ten
+];
